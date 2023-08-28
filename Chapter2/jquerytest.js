9d116d75a69f2e9d26a2545d60ef7866a104bc3c
RyugaHAL12 <ryugahal12@gmail.com>
2023-08-29 01:36:48 +0900

    テキストボックスに入力したテキストを追加するようにした。草は生えない。は？

--- Chapter2/jquerytest.js
+++ Chapter2/jquerytest.js
@@ -2,8 +2,10 @@ let btn = $('#addArea');
 
 $(function(){
 	$('#clickBtn').click(function(){
+		let text = $('#inputWord').val();
+		console.log(text);
 		for(let i = 0; i < 50; i++){
-			btn.append('<p>ボタンが押されました</p>');
+			btn.append('<p>'+ text + '</p>');
 		}
 	})
 })
